ac99aca2848159b9c5232d09a0407dc13f8833ec
Casey Busch <efex32@hotmail.com>
2015-10-12 21:34:44 -0700

    styles and modules

--- server.js
+++ server.js
@@ -1,30 +1,62 @@
-var express = require("express");  //assign identifier to npm package name
+'use strict';
+var express = require('express');
 var app = express();
+var Adjective = require('./lib/adjective.js');
+var Verb = require('./lib/verb.js');
+var Noun = require('./lib/noun.js');
+var getRandomWord = require('./lib/getRandomWord.js');
+var postWord = require('./lib/postWord.js');
+var bodyparser = require('body-parser');
 var port = process.env.PORT || 3000;
 
-var Adjective = function() {
-	this.plaid = true;
-	this.tall = true;
-	this.cranky = true;
-	this.pristine = true;
-	this.eloquent = true;	
-};
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({
+  extended: true
+}));
+
+app.use(express.static(__dirname + '/app/'));
+
 var adjective = new Adjective();
 
-function getRandomWord (object) {
-	var propArray = Object.keys(object);
-	var randomProp = propArray[Math.floor(Math.random() * propArray.length)];
-	return {word: randomProp};
-}
+var verb = new Verb();
 
-app.get("/adjective", function (req, res) {
-	res.send(getRandomWord(adjective));
+var noun = new Noun();
+
+app.get('/adjective', function(req, res) {
+  res.json(getRandomWord(adjective));
 });
 
-app.get("/", function (req, res) {
-	res.send("hello universe");
+app.get('/verb', function(req, res) {
+  res.json(getRandomWord(verb));
+});
+
+app.get('/noun', function(req, res) {
+  res.json(getRandomWord(noun));
+});
+
+app.post('/adjective', function(req, res) {
+  var word = postWord(req.body.word, adjective);
+  res.json(word);
+});
+
+app.post('/verb', function(req, res) {
+  var word = postWord(req.body.word, verb);
+  res.json(word);
+});
+
+app.post('/noun', function(req, res) {
+  var word = postWord(req.body.word, noun);
+  res.json(word);
+});
+
+app.get('/', function(req, res) {
+  res.sendFile('index.html');
 });
 
 app.listen(port, function() {
-	console.log('server started on port ' + port);
+  console.log('server starting. available at http://localhost:' + port);
 });
+
+// app.get('/', function(req, res) {
+//   res.send('hello, universe');
+// });
